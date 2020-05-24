4ca71cc373d5d80331653cd928082590bd31f79d
jpablocr13 <64106716+jpablocr13@users.noreply.github.com>
2020-05-24 09:40:07 -0600

    Add files via upload

---
+++ index/wq-index.js
@@ -0,0 +1,8 @@
+QuizSpecifications[1]=[];
+QuizSpecifications[1].type="single";
+QuizSpecifications[1][0]=false;
+QuizSpecifications[1][1]=false;
+QuizSpecifications[1][2]=false;
+QuizSpecifications[1][3]=true;
+initSession();
+gotoQuestion(1);
